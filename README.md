1e17d5d4ae7919cf6055f127f77d8b68bdc0bb4e
Josafat Martinez <josafatmartinez@users.noreply.github.com>
2025-05-20 17:55:36 -0600

    🎉 feat: initial commit - project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# security_guard_mx
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/security_guard_mx/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.security_guard_mx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
